b25c4ed6744ced13759640037436642223550e01
aayushgore3499 <aayushgore1@gmail.com>
2023-01-24 21:06:17 -0500

    supapsd

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Projects from './components/Projects';
-import LandingPage from './components/LandingPage';
-
-// import 'bootstrap/dist/css/bootstrap.min.css'
-
-{/* <Navbar />
-<div style={{backgroundColor: 'white', marginTop: '250px', marginBottom: '100px', width: '650px', padding: '100px'}}>
-  <Router>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/projects' element={<Projects />} />
-    </Routes>
-  </Router>
-</div> */}
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-    <LandingPage />
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
+
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// import Navbar from './components/Navbar'
+// import Home from './components/Home'
+// import Projects from './components/Projects';
+// import LandingPage from './components/LandingPage';
+
+// // import 'bootstrap/dist/css/bootstrap.min.css'
+
+// {/* <Navbar />
+// <div style={{backgroundColor: 'white', marginTop: '250px', marginBottom: '100px', width: '650px', padding: '100px'}}>
+//   <Router>
+//     <Routes>
+//       <Route path='/' element={<Home />} />
+//       <Route path='/projects' element={<Projects />} />
+//     </Routes>
+//   </Router>
+// </div> */}
+
+// function App() {
+// 	return (
+//     <LandingPage />
+// 	);
+// }
+
+// export default App;
