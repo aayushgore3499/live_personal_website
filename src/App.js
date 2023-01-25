ce8e0aa17b0b9ec7722f18c300d64e0fa0b34a65
aayushgore3499 <aayushgore1@gmail.com>
2023-01-24 21:12:10 -0500

    supapsds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Projects from './components/Projects';
+import LandingPage from './components/LandingPage';
+
 function App() {
   return (
     <div className="App">
@@ -25,24 +31,18 @@ function App() {
 export default App;
 
 
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-// import Navbar from './components/Navbar'
-// import Home from './components/Home'
-// import Projects from './components/Projects';
-// import LandingPage from './components/LandingPage';
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
-// // import 'bootstrap/dist/css/bootstrap.min.css'
-
-// {/* <Navbar />
-// <div style={{backgroundColor: 'white', marginTop: '250px', marginBottom: '100px', width: '650px', padding: '100px'}}>
-//   <Router>
-//     <Routes>
-//       <Route path='/' element={<Home />} />
-//       <Route path='/projects' element={<Projects />} />
-//     </Routes>
-//   </Router>
-// </div> */}
+{/* <Navbar />
+<div style={{backgroundColor: 'white', marginTop: '250px', marginBottom: '100px', width: '650px', padding: '100px'}}>
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/projects' element={<Projects />} />
+    </Routes>
+  </Router>
+</div> */}
 
 // function App() {
 // 	return (
